67670719b156f8605c69a72c35a180fe546387bc
anubhav2100 <121347200+anubhav2100@users.noreply.github.com>
2024-07-01 14:28:45 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'anubhav-job-board.s3.amazonaws.com',
-        },
-      ],
-    },
     eslint: {
       ignoreDuringBuilds : true,
     },
